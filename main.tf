1797b0e62a1982c9fde6159099d9df0f8e532cb7
Todor Peychinov <161106514+todorpeychinov@users.noreply.github.com>
2026-04-05 15:04:16 +0300

    formated main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name = "StorageRG"
+    resource_group_name  = "StorageRG"
     storage_account_name = "taskboardstoragetp2026"
-    container_name = "taskboardcontainer"
-    key = "terraform.tfstate"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
   }
 }
 
